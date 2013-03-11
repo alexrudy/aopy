761a147b2b9c37422b661b201d274a666491e1ec
Alex Rudy <alex.rudy@gmail.com>
2013-03-11 12:25:31 -0700

    Plotting scripts use pyshell

--- plot_luke_wind.py
+++ plot_luke_wind.py
@@ -11,15 +11,55 @@ from __future__ import division
 import matplotlib.pyplot as plt
 import numpy as np
 import pyfits as pf
-import os.path
+import os.path, glob
 
-def plot_polar_map(X,Y,nbins=1e3,Rmax=None):
+from pyshell.util import query_string, is_type_factory, check_exists
+from pyshell.base import CLIEngine
+
+def plot_histogram_binning(XY,nbins=1e2,size=None):
+    """Plot a histogram binned wind map"""
+    magnitude = np.sqrt(np.sum(np.power(XY,2),axis=1))
+    X,Y = XY[:,0], XY[:,1]
+    size = size if not size is None else np.max(magnitude)
+    H, xedges, yedges = np.histogram2d(X,Y,bins=nbins,range=[[-size,size],[-size,size]])
+    extent = [xedges[0], xedges[-1], yedges[0], yedges[-1] ]
+    plt.imshow(H.T,extent=extent,interpolation='nearest',origin='lower')
+    plt.colorbar()
+    plt.ylabel("Wind Velocity (m/s)")
+    plt.xlabel("Wind Velocity (m/s)")
+
+def plot_time_series(T,XY,clip=1e3,swindow='flat',wsize=101):
+    """docstring for plot_time_series"""
+    assert T.shape[0] == XY.shape[0], "Shape Mismatch"
+    if clip > T.shape[0]:
+        clip = slice()
+    else:
+        clip = slice(None,clip)
+    magnitude = np.sqrt(np.sum(np.power(XY,2),axis=1))
+    tc = T[clip]
+    xc = XY[clip,0]
+    yc = XY[clip,1]
+    mc = magnitude[clip]
+    plt.plot(tc,xc,'g.',label="x-wind")
+    plt.plot(tc,smooth(xc,wsize,window=swindow),'g-',linewidth=2.0)
+    plt.plot(tc,yc,'b.',label="y-wind")
+    plt.plot(tc,smooth(yc,wsize,window=swindow),'b-',linewidth=2.0)
+    plt.plot(tc,mc,'r.',label='wind magnitude',zorder=0.1,alpha=0.5)
+    plt.plot(tc,smooth(mc,wsize,window=swindow),'r-',linewidth=2.0,zorder=0.1,alpha=0.5)
+    plt.legend()
+    plt.xlabel("Time (s)")
+    plt.ylabel("Wind Velocity (m/s)")
+
+def plot_polar_map(XY,Rmax=None,Rmin=None):
     """Make a polar histogram map"""
+    X, Y = XY[:,0], XY[:,1]
     R,Theta = np.sqrt(X**2.0+Y**2.0), np.arctan2(Y,X)
     if Rmax is None:
         Rmax = np.max(R)
+    if Rmin is None:
+        Rmin = np.min(R)+1e-14
     thedges = np.linspace(-np.pi,np.pi,100,endpoint=True)
-    redges = np.concatenate((np.array([0]),np.logspace(np.log(np.min(R)),np.log(Rmax),99,endpoint=True)))
+    redges = np.linspace(np.min(R),np.max(R),100,endpoint=True)
     H, thedges, redges  = np.histogram2d(Theta,R,bins=[thedges,redges])
     thcoords, rcoords = np.meshgrid(thedges,redges)
     dthetas = thedges[1:] - thedges[:-1]
@@ -31,6 +71,9 @@ def plot_polar_map(X,Y,nbins=1e3,Rmax=None):
     # print u"θ=",np.max(thcoords)/np.pi,u"π",u" , ",np.min(thcoords)/np.pi,u"π"
     plt.axes(polar=True)
     plt.pcolormesh(thcoords,rcoords,H.T)
+    plt.colorbar()
+    plt.xlabel("Wind Velocity (m/s)")
+    plt.gca().tick_params(axis='x', colors='w')
     
 def smooth(x,window_len=11,window='hanning'):
         if x.ndim != 1:
@@ -49,64 +92,81 @@ def smooth(x,window_len=11,window='hanning'):
         y=np.convolve(w/w.sum(),s,mode='same')
         return y[window_len:-window_len+1]
 
-FileName = "data/keck/proc/20070730_2_wind.fits"
-# FileName = "data/altair/proc/20070417_wind.fits"
-Method = 0
-Methods = ['GN','RT','2D','XY']
-BaseName = os.path.splitext(os.path.basename(FileName))[0]
-InstName = FileName.split("/")[1]
-OBaseName = os.path.join("figures",InstName+"-"+BaseName+"-"+Methods[Method])
-DataFile = pf.open(FileName)
-WIND = DataFile[0].data[:,:,Method]
-WINDSQ = np.power(WIND,2)
-WINDX = WIND[:,0]
-WINDY = WIND[:,1]
-WINDMAG = np.sqrt(WINDSQ[:,0]+WINDSQ[:,1])
-TIME = WIND[:,2]
-clip = 2e3
-plt.plot(TIME[:clip],WINDX[:clip],'g.',label="x-wind")
-plt.plot(TIME[:clip],smooth(WINDX[:clip],101,window='flat'),'g-',linewidth=2.0)
-plt.plot(TIME[:clip],WINDY[:clip],'b.',label="y-wind")
-plt.plot(TIME[:clip],smooth(WINDY[:clip],101,window='flat'),'b-',linewidth=2.0)
-plt.plot(TIME[:clip],WINDMAG[:clip],'r.',label='wind magnitude',zorder=0.1,alpha=0.5)
-plt.plot(TIME[:clip],smooth(WINDMAG[:clip],101,window='flat'),'r-',linewidth=2.0,zorder=0.1,alpha=0.5)
+def select_files(files,allow_all=True):
+    """docstring for select_files"""
+    print "Select file to examine:"
+    if allow_all:
+        print "{:>3d}) {:s}".format(0,"all")
+    for i,filename in enumerate(files):
+        print "{:>3d}) {:s}".format(i+1,filename)
+    value = int(query_string("Select File:",default=0,validate=is_type_factory(int))) - 1
+    if allow_all and value == -1:
+        return files
+    else:
+        return [files[value]]
 
-plt.legend()
-plt.xlabel("Time (s)")
-plt.ylabel("Wind Velocity (m/s)")
-plt.title("Wind Estimation for Keck-0 with Gauss-Newton Method")
-plt.savefig(OBaseName+"-Timeseries.png")
+class PlotLukeWind(CLIEngine):
+    """docstring for PlotLukeWind"""
+    defaultcfg = False
+    module = __name__
+    
+    def configure(self):
+        """Initialize un-configured arguments"""
+        super(PlotLukeWind, self).configure()
+        self.parser.add_argument("name",help="File name or index",action='store',nargs="*")
+        
+        
+        
+    def do(self):
+        """Take action!"""
+        files = glob.glob("data/**/proc/*_wind.fits")
+        if len(self.opts.name) == 0:
+            self.opts.name = select_files(files)
+        if len(self.opts.name) == 1 and is_type_factory(int)(self.opts.name[0]) and int(self.opts.name[0]) == 0:
+            self.opts.name = files
+        FileNames = []
+        for fname in self.opts.name:
+            if is_type_factory(int)(fname):
+                FileNames.append(files[fname])
+            elif isinstance(fname,basestring) and check_exists(fname):
+                FileNames.append(fname)
+            else:
+                print("Skipping File '{}'".format(fname))
+        
+        print("Examining:\n -{}".format("\n -".join(FileNames)))
+        Methods = ['GN','RT','2D','XY']
+        
+        for FileName in FileNames:
+            BaseName = os.path.splitext(os.path.basename(FileName))[0]
+            InstName = FileName.split("/")[1]
+            OBaseName = os.path.join("figures","{type}_{method}_{tel}_{set}.{ext}")
+            OBaseDict = {
+                'tel' : InstName,
+                'set' : "_".join(BaseName.split("_")[:-1]),
+                'set_tex' : "\\verb+{}+".format("_".join(BaseName.split("_")[:-1])),
+                'ext' : "png"
+            }
 
-plt.clf()
-plt.plot(WIND[:clip,0],WIND[:clip,1],'.')
-plt.axis('equal')
-plt.savefig(OBaseName+"-ScatterPlot.png")
-plt.clf()
-
-dist = np.max(WINDMAG)
-H, xedges, yedges = np.histogram2d(WIND[:,0],WIND[:,1],bins=100,range=[[-dist,dist],[-dist,dist]])
-extent = [xedges[0], xedges[-1], yedges[0], yedges[-1] ]
-plt.imshow(H.T,extent=extent,interpolation='nearest',origin='lower')
-plt.colorbar()
-plt.title("Wind Velocity Density Map")
-plt.ylabel("Wind Velocity (m/s)")
-plt.xlabel("Wind Velocity (m/s)")
-plt.savefig(OBaseName+"-WindMap-XY.png")
-
-plot_polar_map(WINDX,WINDY,Rmax=dist)
-plt.grid(color='w')
-plt.colorbar()
-plt.xlabel("Wind Velocity (m/s)")
-plt.gca().tick_params(axis='x', colors='w')
-plt.savefig(OBaseName+"-PolarMap.png")
-plt.clf()
-plot_polar_map(smooth(WINDX,11,window='flat'),smooth(WINDY,11,window='flat'),Rmax=dist)
-plt.gca().spines['polar'].set_color('w')
-plt.grid(color='w')
-plt.xlabel("Wind Velocity (m/s)")
-plt.gca().tick_params(axis='x', colors='w')
-plt.colorbar()
-plt.savefig(OBaseName+"-BinPolarMap.png")
-
-print OBaseName
-
+            DataFile = pf.open(FileName)
+            for Method in range(len(Methods)):
+                WIND = DataFile[0].data[:,:2,Method]
+                TIME = DataFile[0].data[:,2,Method]
+                clip = 2e3
+                OBaseDict["method"] = Methods[Method]
+                print("Plotting {method} for {tel} during '{set}'".format(**OBaseDict))    
+                plot_polar_map(WIND)
+                plt.title("Polar Map of wind from {method} method at {tel} for '{set_tex}'".format(**OBaseDict))
+                plt.savefig(OBaseName.format(type="polarmap",**OBaseDict))
+                plt.clf()
+                plot_time_series(TIME,WIND)
+                plt.title("Time Series wind from {method} method at {tel} for '{set_tex}'".format(**OBaseDict))
+                plt.savefig(OBaseName.format(type="timeseries",**OBaseDict))
+                plt.clf()
+                plot_histogram_binning(WIND)
+                plt.title("Wind Velocitiy Histogram from {method} method at {tel} for '{set_tex}'".format(**OBaseDict))
+                plt.savefig(OBaseName.format(type="histogram",**OBaseDict))
+                plt.clf()
+                
+if __name__ == '__main__':
+    PlotLukeWind.script()
+    
