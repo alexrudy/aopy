9f93d81df75e836fac32fc531237ffd46328a90e
Alex Rudy <alex.rudy@gmail.com>
2013-05-01 13:38:04 -0700

    [DEV] Support for SubHarmonics

--- aopy/atmosphere/screen.py
+++ aopy/atmosphere/screen.py
@@ -48,14 +48,14 @@ def generate_filter(shape,r0,du,L0=None,nsh=0):
             shf[4,i] = (dkx**2+k0**2)**(-11./12.)*np.sqrt(dkx*dky)
             shf[5,i] = k2**(-11./12.)*np.sqrt(dkx*dky)
             shf[6,i] = (dky**2+k0**2)**(-11./12.)*np.sqrt(dkx*dky)
-            shf[7,i] = k2**(-11./12.)*sqrt(dkx*dky)
+            shf[7,i] = k2**(-11./12.)*np.sqrt(dkx*dky)
         shf = shf*np.sqrt(0.023)*(2*np.pi)**(5./6.)*r0**(-5./6.)
     else:
         shf = None
     
     return f, shf
 
-def generate_screen_with_noise(f,noise=None,shf=None,du=1.0):
+def generate_screen_with_noise(f,noise=None,shf=None,shn=None,du=1.0):
     """
     Generate a screen from a given grid of noise.
     
@@ -67,31 +67,31 @@ def generate_screen_with_noise(f,noise=None,shf=None,du=1.0):
     rn = noise if noise is not None else np.ones(f.shape)
     frn = numpy.fft.fftshift(numpy.fft.fft2(rn)) * np.sqrt(np.prod(f.shape))
     s = np.real(numpy.fft.ifft2(numpy.fft.ifftshift(frn*f)))
+    if shf is not None:
+        n,m = f.shape
+        dkx = 2.0*np.pi/(n*du)
+        dky = 2.0*np.pi/(m*du)
+        x = (np.arange(n)*du) # (fltarr(m)+1)
+        y = (np.arange(n)+1) # (findgen(m)*du)
+        rn = np.ones((8,),dtype=np.complex)
+        nsh = shf.shape[0]
+        for i in range(nsh):            
+            rn[0:4] = shn
+            rn[7] = np.conj(rn[0])
+            rn[6] = np.conj(rn[1])
+            rn[5] = np.conj(rn[2])
+            rn[4] = np.conj(rn[3])
+            dkx = dkx/3.0
+            dky = dky/3.0
+            s = s + rn[0]*shf[0,i]*np.exp(1j*(-dkx*x-dky*y))
+            s = s + rn[1]*shf[1,i]*np.exp(1j*(-dky*y))
+            s = s + rn[2]*shf[2,i]*np.exp(1j*(dkx*x-dky*y))
+            s = s + rn[3]*shf[3,i]*np.exp(1j*(-dkx*y))
+            s = s + rn[4]*shf[4,i]*np.exp(1j*(dkx*y))
+            s = s + rn[5]*shf[5,i]*np.exp(1j*(-dkx*x+dky*y))
+            s = s + rn[6]*shf[6,i]*np.exp(1j*(dky*y))
+            s = s + rn[7]*shf[7,i]*np.exp(1j*(dkx*x+dky*y))
     
-    # if shf is not None:
-    #     dkx = 2.0*np.pi/(n*du)
-    #     dky = 2.0*np.pi/(m*du)
-    #     x = (np.arange(n)*du) # (fltarr(m)+1)
-    #     y = (np.arange(n)+1) # (findgen(m)*du)
-    #     rn = np.ones((8),dtype=np.complex)
-    #     if (size(shf))(0) eq 1 then nsh = 1 else nsh = (size(shf))(2)
-    #     for j=0,nsh-1 do begin
-    #         rn[0:3] = (randomn(seed,4) + i*randomn(seed,4))/sqrt(2.)
-    #         rn[7] = conj(rn[0])
-    #         rn[6] = conj(rn[1])
-    #         rn[5] = conj(rn[2])
-    #         rn[4] = conj(rn[3])
-    #         dkx = dkx/3.
-    #         dky = dky/3.
-    #         s = s + rn[0]*shf[0,j]*exp(i*(-dkx*x-dky*y))
-    #         s = s + rn[1]*shf[1,j]*exp(i*(-dky*y))
-    #         s = s + rn[2]*shf[2,j]*exp(i*(dkx*x-dky*y))
-    #         s = s + rn[3]*shf[3,j]*exp(i*(-dkx*y))
-    #         s = s + rn[4]*shf[4,j]*exp(i*(dkx*y))
-    #         s = s + rn[5]*shf[5,j]*exp(i*(-dkx*x+dky*y))
-    #         s = s + rn[6]*shf[6,j]*exp(i*(dky*y))
-    #         s = s + rn[7]*shf[7,j]*exp(i*(dkx*x+dky*y))
-    # 
     return s
     
 
@@ -107,17 +107,17 @@ def generate_screen(f,seed=None,shf=None,du=None):
     """
     import numpy.random
     rn = numpy.random.RandomState(seed).randn(*f.shape)
-    return generate_screen_with_noise(f,rn,shf,du)
+    shn = (numpy.random.RandomState(seed).randn(4) + 1j*numpy.random.RandomState(seed).randn(4))/np.sqrt(2.0)
+    return generate_screen_with_noise(f,rn,shf,shn,du)
 
 class Screen(object):
     """A base phase screen class.
     
-    :param shape: The shape of the screen (x,y) tuple.
-    :param r0: $r_0$ fried parameter for the screen.
-    :param L0: $L_0$ outer scale for the screen.
-    :param nsh:
-    :param shf:
-    :param du: Pixels per subaperture
+    :param tuple shape: The shape of the screen (x,y) tuple.
+    :param float r0: $r_0$ fried parameter for the screen.
+    :param float L0: $L_0$ outer scale for the screen.
+    :param int nsh: Number of subharmonics. (default``=0`` for no subharmonics)
+    :param float du: Pixels per subaperture
     
     """
     def __init__(self, shape, r0, seed = None, du=1.0, L0=None,nsh=0):
