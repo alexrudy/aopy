047059ff7522b995f945f1a6888bcac46de9a6b7
Alex Rudy <alex.rudy@gmail.com>
2013-04-30 21:25:48 -0700

    [DEV] BlowingScreen .all attributes
    .all attribute contains the full phase-wrap filled.

--- aopy/util/math.py
+++ aopy/util/math.py
@@ -51,7 +51,22 @@ def edgemask(aperture):
     result = scipy.signal.convolve(aperture,kernel,mode='same')
     return result >= 9
     
-    
-    
+def smooth(x,window_len=11,window='hanning'):
+        if x.ndim != 1:
+                raise ValueError, "smooth only accepts 1 dimension arrays."
+        if x.size < window_len:
+                raise ValueError, "Input vector needs to be bigger than window size."
+        if window_len<3:
+                return x
+        if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+                raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
+        s=np.r_[2*x[0]-x[window_len-1::-1],x,2*x[-1]-x[-1:-window_len:-1]]
+        if window == 'flat': #moving average
+                w=np.ones(window_len,'d')
+        else:  
+                w=getattr(np,window)(window_len)
+        y=np.convolve(w/w.sum(),s,mode='same')
+        return y[window_len:-window_len+1]
+
     
         
