01d7c552e1857158c4133c87de49e600957ef9d6
Alex Rudy <alex.rudy@gmail.com>
2013-05-02 17:26:25 -0700

    [DEV] Module Level Documentation

--- aopy/aperture/__init__.py
+++ aopy/aperture/__init__.py
@@ -6,5 +6,9 @@
 #  Created by Alexander Rudy on 2013-04-29.
 #  Copyright 2013 Alexander Rudy. All rights reserved.
 # 
+"""
+This module is documented in ``docs/aperture.rst``
+"""
+
 
 from .core import *
