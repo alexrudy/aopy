109f3785e0b6fb6b747a81746d56ef66b46a0986
Alex Rudy <alex.rudy@gmail.com>
2013-05-12 19:11:31 -0700

    [DEV] Examples and Docs

---
+++ examples/gn_plan.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# 
+#  gn_plan.py
+#  aopy
+#  
+#  Created by Jaberwocky on 2013-05-10.
+#  Copyright 2013 Jaberwocky. All rights reserved.
+# 
+from __future__ import (absolute_import, unicode_literals, division,
+                        print_function)
+
+import numpy as np
+import os, os.path
+
+import pidly
+
+from aopy.atmosphere import wind
+from wcao.estimators.gaussnewton import GaussNewtonEstimator
+from wcao.data.core import WCAOCase
+from astropy.utils.console import ProgressBar
+from astropy.io import fits
+from pyshell.config import StructuredConfiguration
+from pyshell.loggers import getSimpleLogger
+from pyshell.util import ipydb
+
+ipydb()
+log = getSimpleLogger(__name__)
+
+log.status("Loading Data")
+Data = WCAOCase("Keck","20070730_2",(WCAOCase.__module__,'telemetry.yml'))
+Data.telemetry.console = True
+Data.telemetry.load_raw()
+Plan = GaussNewtonEstimator().setup(Data)
+
+for n in ProgressBar(Plan.nt):
+    Plan.estimate()
+Plan.finish()
+
+import matplotlib.pyplot as plt
+fig = plt.figure()
+Data.results["GN"].threepanelts(fig)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+Data.results["GN"].map(ax)
+plt.show()
---
+++ examples/play_phase.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 
+#  play_phase.py
+#  aopy
+#  
+#  Created by Alexander Rudy on 2013-05-03.
+#  Copyright 2013 Alexander Rudy. All rights reserved.
+# 
+
+from __future__ import (absolute_import, unicode_literals, division,
+                        print_function)
+
+import pyshell
+
+class PhasePlayer(pyshell.CLIEngine):
+    """Play me some phase!"""
+    
+    defaultcfg = False
+    
+    def init(self):
+        """Initialize!"""
+        super(PhasePlayer, self).init()
+        self.parser.add_argument('-f',dest='fitsfile', help="The input FITS file.", metavar="file.fits")
+        self.parser.add_argument('-o',dest="movie",default="blowing_screen.mp4")
+        
+    def do(self):
+        """Run the phase screen."""
+        import matplotlib
+        import matplotlib.pyplot as plt
+        from matplotlib import animation
+        import numpy as np
+        from astropy.io import fits
+        
+        self.ffile = fits.open(self.opts.fitsfile)
+        self.ntime = self.ffile[0].data.shape[0]
+        self.vlim = (np.min(self.ffile[0].data),np.max(self.ffile[0].data))
+        
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(111)
+        self.I = self.ax.imshow(self.ffile[0].data[0,...],interpolation='nearest',vmin=self.vlim[0],vmax=self.vlim[1])
+        self.fig.colorbar(self.I)
+        self.ax.set_title("Phase at t=%5d/%5d" % (0,self.ntime))
+        self.anim = animation.FuncAnimation(self.fig, self.animate, frames=500, interval=20)
+        self.anim.save(self.opts.movie, fps=30, extra_args=['-vcodec', 'libx264'],
+        writer='ffmpeg_file',)
+        # plt.show()
+        
+    def animate(self,t):
+        """Animate the screen!"""
+        frame = self.ffile[0].data[t,...]
+        self.I.set_data(frame)
+        self.ax.set_title("Phase at t=%5d/%5d" % (t,self.ntime))
+        
+    
+
+if __name__ == '__main__':
+    PhasePlayer.script()
