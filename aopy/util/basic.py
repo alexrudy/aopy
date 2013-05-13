ee2d02608a601122fcb212219d8f1e148173cfd2
Alex Rudy <alex.rudy@gmail.com>
2013-05-13 13:40:07 -0700

    [DEV] Changed consolecontext to involve pseudo-looper

--- aopy/util/basic.py
+++ aopy/util/basic.py
@@ -20,26 +20,30 @@ class ConsoleContext(object):
     """Allow a switch between range and progress-bar"""
     def __init__(self):
         super(ConsoleContext, self).__init__()
-        self._range = range
-        self._console = False
+        self._looper = self._pseudoloop
+        self.console = True
     
     @property
-    def range(self):
+    def looper(self):
         """The range function"""
-        return self._range
+        return self._looper
     
     @property
     def console(self):
         """Whether to use the builtin range or a progressbar"""
         return self._console
         
+    def _pseudoloop(self,iterator):
+        """docstring for _pseudoloop"""
+        return iterator
+        
     @console.setter
     def console(self,value):
         """docstring for console"""
         if value:
             from astropy.utils.console import ProgressBar
-            self._range = ProgressBar
+            self._looper = ProgressBar
         else:
-            self._range = range
+            self._looper = self._pseudoloop
         self._console = value
         
