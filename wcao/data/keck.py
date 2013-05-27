287f537dcf37cf0f876dd66c94b2ac0ede83cd53
Alex Rudy <alex.rudy@gmail.com>
2013-05-27 11:40:11 +0200

    Keck Transfer Function

--- wcao/data/keck.py
+++ wcao/data/keck.py
@@ -51,5 +51,56 @@ def disp2d(data):
     elif data.size == 6400:
         output = np.reshape(data,(80,80))
     return output
+    
+
+"""
+The IDL Code ported by transfac is::
+    
+    ;;; This is the influence function filter of keck's DM.
+    ;;; taken from telemetry_analysis_07/get_dm_transfer_function.pro
+    w1 = 2
+    w2 = -1
+    sig1 = 0.54
+    sig2 = 0.85
+    ;    kfac = 0.47
+    kfac = 1. ;;; already folded in the get_processed_data_keck
+    m = 8.
+    myx = rebin(findgen(n*m) - n*m/2, n*m, n*m)*1./m
+    myy = transpose(myx)
+
+    influence_function = kfac*(w1/(2*!pi*sig1^2)*exp(-0.5*(myx^2 + myy^2)/sig1^2) + $
+                             w2/(2*!pi*sig2^2)*exp(-0.5*(myx^2 + myy^2)/sig2^2))
+
+    bigtf = shift(real_part(fft(shift(influence_function, n*M/2, n*m/2)))*n^2, n*M/2, n*m/2)
+    tf = shift(bigtf[n*m/2-n/2: n*m/2+n/2-1, n*m/2-n/2: n*m/2+n/2-1], n/2, n/2)
+    dmtrans_mulfac = abs(tf)^2
+    
+  
+"""
+
+def transfac():
+    """Keck Transfer Function. See the IDL Code above to understand this data."""
+    import scipy.fftpack
+    w1 = 2.0
+    w2 = -1.0
+    sig1 = 0.54
+    sig2 = 0.85
+    kfac = 1.0
+    m = 8
+    n = 26
+    
+    mn = n * m
+    mns = mn // 2
+    
+    x,y = np.mgrid[-1*mns:mns,-1*mns:mns]
+    x,y = x.astype(np.float)/m, y.astype(np.float)/m
+    influence_function = kfac * (w1 / (2 * np.pi * sig1**2.0) * np.exp(-0.5 * (x**2.0 + y**2.0)/sig1**2.0)) + (w2 / (2 * np.pi * sig2**2.0) * np.exp(-0.5 * (x**2.0 + y**2.0)/sig2**2.0))
+    bigtf = scipy.fftpack.ifftshift(np.real(scipy.fftpack.ifft2(scipy.fftpack.fftshift(influence_function)))) * n ** 2.0
+    tf = bigtf[n*m/2-n/2: n*m/2+n/2, n*m/2-n/2: n*m/2+n/2]
+    tf = np.roll(np.roll(tf,int(n//2),axis=0),int(n//2),axis=1)
+    return np.abs(tf)**2.0
+    
+    
+    
 
     
