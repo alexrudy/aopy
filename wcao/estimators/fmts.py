49c47e77cb066f94c97d2c562ab2cce2ab605343
Alex Rudy <alex.rudy@gmail.com>
2013-07-15 16:41:08 -0700

    [DEV] FMTS cleanup and refactoring.
    - Basic utilities (for converting tables/grids/list of dictionaries) are now in .fmtsutil
    - Better handling of configuration values.

--- wcao/estimators/fmts.py
+++ wcao/estimators/fmts.py
@@ -60,8 +60,8 @@ Supporting Functions
 .. autofunction::
     recenter
 
-Peak Grid Functions
-*******************
+Peak Grid Functions - :mod:`FMTSutil`
+*************************************
 
 .. autofunction::
     peaks_from_table
@@ -89,6 +89,8 @@ from aopy.util.basic import ConsoleContext
 from pyshell.core import Struct
 from pyshell.config import DottedConfiguration
 from pyshell import getLogger
+
+from .fmtsutil import peaks_to_table, peaks_array_from_grid, peaks_from_table, layers_to_table
 
 def periodogram(data, periodogram_length, window=None, half_overlap=True):
     """Make a periodogram from N-dimensional data. The periodogram will be made across axis 0 of the data, and returned in axis 0 of the result. The periodogram is windowed by default. A custom window (which should be the same size as the expected output data) can be passed in at the window parameter.
@@ -291,90 +293,7 @@ def fitter(x,alpha,peak,center):
     """
     f = peak/(1 - 2*alpha*np.cos(x-center) + alpha**2.0)
     return f
-    
-def peaks_to_table(peakgrid,npeaks=None):
-    """Create a table from a grid of peaks.
-    
-    The grid of peaks should be a ``k`` x ``l`` array of lists of peak properties.
-    The table will be a record array with columns ``k``, ``l``, :math:`\\alpha`, :math:`\omega`, power, and fit ``rms``.
-    
-    :param ndarray peakgrid: The grid of peaks to form into a table.
-    :param int npeaks: The total number of peaks in the grid. If it isn't provided, it will be found, in order `N`.
-    :return: Record array of peaks.
-    
-    This function is the inverse of :func:`peaks_from_table`.
-    
-    """
-    if npeaks is None:
-        npeaks = sum([ len(x) for x in peakgrid.flat ])
-    k = np.zeros((npeaks,),dtype=np.int) 
-    l = np.zeros((npeaks,),dtype=np.int)
-    alpha = np.zeros((npeaks,),dtype=np.float)
-    omega = np.zeros((npeaks,),dtype=np.float)
-    power = np.zeros((npeaks,),dtype=np.float)
-    rms = np.zeros((npeaks,),dtype=np.float)
-    
-    pol = 0
-    for k_i,l_i in itertools.product(*map(range,peakgrid.shape)):
-        for layer in peakgrid[k_i,l_i]:
-            k[pol] = k_i
-            l[pol] = l_i
-            alpha[pol] = layer["alpha"]
-            omega[pol] = layer["omega"]
-            power[pol] = layer["variance"]
-            rms[pol] = layer["rms"]
-            pol += 1
-    return np.rec.fromarrays([k,l,alpha,omega,power,rms],names=["k","l","alpha","omega","power","rms"])
-    
-def peaks_from_table(table,shape):
-    """Convert a peaks-table back to a peak grid.
-    
-    This will result in a grid ``k``x``l`` of peaks, where each element in the gird is a list of peak properties. Each peak property is a dictionary with keys ``'alpha','omega','variance','rms'``.
-    
-    :param ndarray table: A record array with columns ``k``, ``l``, :math:`\\alpha`, :math:`\omega`, power, and fit ``rms``.
-    :param tuple shape: The shape of output grid, which should be the maximum ``k`` and ``l`` to be inserted.
-    :return: ``peaks_grid, npeaks`` where the grid is as specified above, and ``npeaks`` is a grid with the number of peaks in each element stored in that element.
-    
-    Note that ``k`` and ``l`` are indicies, not spatial frequencies, and so are always between 0 and their respective maxima.
-    
-    This function is the inverse of :func:`peaks_to_table`.
-    
-    """
-    peaks_grid = np.empty(shape,dtype=object)
-    npeaks = np.zeros(shape,dtype=np.int)
-    for k_i,l_i in itertools.product(*map(range,shape)):
-        select = (table['k'] == k_i) & (table['l'] == l_i)
-        peaks = []
-        for peak in table[select]:
-            peaks.append({
-            'alpha' : peak['alpha'],
-            'omega' : peak['omega'],
-            'variance' : peak['power'],
-            'rms' : peak['rms']
-            })
-        peaks_grid[k_i,l_i] = peaks
-        npeaks[k_i,l_i] = len(peaks)
-    return peaks_grid, npeaks
-    
-def peaks_array_from_grid(peaks,npeaks=None):
-    """The function turns a grid of peaks into a multidimensional array of peaks.
-    
-    The array will have shape ``(k,l,n,4)`` where ``n`` is the largest number of peaks in any ``k,l`` mode.
-    
-    :param ndarray peaks: The grid of peaks from which to draw items.
-    :param ndarray npeaks: An array specifying the number of peaks at each ``k,l`` position. If ``None``, it will be calculated.
-    :return: A 4-D grid of peak parameters. The dimensions are ``k,l,n,p`` where p is the parameter, stored as ``[alpha,omega,variance,rms]``.
-    
-    """
-    if npeaks is None:
-        npeaks = np.reshape([len(x) for x in peaks.flat],peaks.shape)
-    peaks_grid = np.zeros(peaks.shape + (np.max(npeaks),4))
-    for k,l in itertools.product(*map(range,peaks.shape)):
-        if npeaks[k,l] > 0:
-            peaks_grid[k,l,:npeaks[k,l],:] = np.array([ [peak["alpha"], peak["omega"], peak["variance"], peak["rms"] ] for peak in peaks[k,l]])
-    return peaks_grid
-    
-    
+
 def create_layer_metric(peaks,npeaks,omega,klshape,rate,maxv=100,deltav=5,frac=0.5,lowest_hz=2.0,dist_hz=1.0,D=1):
     """Create a layer metric for finding individual layers.
 
@@ -544,15 +463,9 @@ def find_layers(metric,vv,spacing=10,min_layer_threshold=0.75,centroid=None):
             com_y, com_x = recenter(peak_x,peak_y,metric,centroid)
             vx = np.interp(com_x,np.arange(vv.shape[0]),vv)
             vy = -1*np.interp(com_y,np.arange(vv.shape[0]),vv)
-            pvx = vx + 0.25
-            pvy = vy# - 0.25
             layers.append({
                 'vx' : vx,
                 'vy' : vy,
-                'ivx' : peak_x,
-                'ivy' : peak_y,
-                'pvx': pvx,
-                'pvy': pvy,
                 'm' : metric[peak_x,peak_y],
             })
             layer_pos.append([peak_x,peak_y])
@@ -725,38 +638,6 @@ class FourierModeEstimator(BaseEstimator):
             self.log.info("Masking %d noisy positions from PSDs" % np.sum(mask))
             self.psd[mask] = 0.0
         
-    def _save_periodogram(self,filename,clobber=False):
-        """Save the periodogram to a fits file.
-        
-        The primary HDU contains the full ``t,k,l`` periodogram. The secondary HDU contains the frequency scale for this data. The secondary HDU could be recreated using the ``rate`` keyword in the primary header. The periodogram is stored with four axes, so reconstruction the data can be done like this::
-            
-            data = fitsdata[0] + 1j * fitsdata[1]
-            
-        
-        """
-        from astropy.io import fits
-        psd = np.array([np.real(self.psd),np.imag(self.psd)])
-        HDU = fits.PrimaryHDU(psd)
-        HDU.header['CaseName'] = self.case.casename
-        HDU.header['rate'] = self.rate
-        HDU_hz = fits.ImageHDU(self.hz)
-        HDUList = fits.HDUList([HDU,HDU_hz])
-        HDUList.writeto(filename,clobber=clobber)
-        
-    def _load_periodogram(self,filename):
-        """Load the periodogram from a fits file.
-        
-        This function loads periodograms which were saved by :meth:`_save_periodogram`.
-        
-        """
-        from astropy.io import fits
-        
-        with fits.open(filename) as FitsFile:
-            self.psd = FitsFile[0].data[0].copy() + 1j * FitsFile[0].data[1].copy()
-            self.rate = np.copy(FitsFile[0].header['rate'])
-            length = self.psd.shape[0]
-            self.hz = FitsFile[1].data.copy()
-        
     def _create_peak_template(self):
         """Make peak templates for correlation fitting. The peak template occurs for a PSD with all ones.
         
@@ -816,19 +697,6 @@ class FourierModeEstimator(BaseEstimator):
         self.npeaks[k,l] = len(peaks)
         return peaks
         
-    
-    def _save_peaks_to_table(self,filename,clobber=False):
-        """Save found peaks to a table. The fits table is a simple way of storing the results from :func:`peaks_to_table`.
-        """
-        from astropy.io import fits
-        tbl_hdu = fits.new_table(peaks_to_table(self.peaks,np.sum(self.npeaks)))
-        tbl_hdu.writeto(filename,clobber=clobber)
-        
-    def _read_peaks_from_table(self,filename):
-        """Loads found peaks from a fits file saved in the format of :meth:`_save_peaks_to_table`."""
-        from astropy.io import fits
-        with fits.open(filename) as FitsFile:
-            self.peaks, self.npeaks = peaks_from_table(FitsFile[1].data,self.psd.shape[1:])
         
     def _fit_peaks_to_metric(self):
         """Fit each peak to a metric. The metric fitting is done by :func:`create_layer_metric`.
