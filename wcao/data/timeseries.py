7e1fd2326a44ff58056f9f9bc163c1fa01de2779
Alex Rudy <alex.rudy@gmail.com>
2013-07-30 13:00:45 -0700

    [DEV] Improved data structures for WCAO results

--- wcao/data/timeseries.py
+++ wcao/data/timeseries.py
@@ -10,35 +10,52 @@ from __future__ import (absolute_import, unicode_literals, division,
                         print_function)
 
 import numpy as np
+import astropy.units as u
 
 import collections
 
 from .estimator import WCAOEstimate
 
+from aopy.util.units import ensure_quantity
+
 
 class WCAOTimeseries(WCAOEstimate):
     """A representation of WCAO timeseries data"""
     def __init__(self,*args,**kwargs):
-        self._timestep = kwargs.pop('timestep',1.0)
         super(WCAOTimeseries, self).__init__(*args, **kwargs)
         self.clip = False
         
-    def _init_data(self,data):
+    def init_data(self,data=None,time=None,timestep=None):
         """Initialize and validate timeseires data"""
         if data is None:
             return
         data = np.array(data)
         if data.ndim != 3:
-            raise ValueError("{0:s}-type data should have 3 dimensions: (time,layer,x/y). data.ndim={1.ndim:d}".format(self._arraytype,data))
+            raise ValueError("{0:s}-type data should have 3 dimensions: (time,layer,x/y). data.ndim={1.ndim:d}".format(self.arraytype,data))
         if data.shape[2] != 2:
-            raise ValueError("{0:s}-type data should have shape (ntime,nlayers,2(x/y)). data.shape={1.shape!r}".format(self._arraytype,data))
+            raise ValueError("{0:s}-type data should have shape (ntime,nlayers,2(x/y)). data.shape={1.shape!r}".format(self.arraytype,data))
         self._data = data
+        
+        if timestep is None:
+            self._timestep = ensure_quantity(timestep,u.s)
+        
+        if time is None:
+            self._time = np.arange(self.ntime) * self._timestep.to('s')
+        else:
+            time = ensure_quantity(time, u.s)
+            if time.ndim != 1 or time.shape[0] != self._data.shape[0]
+                raise ValueError("{0:s}-type time should match the shape of the first axes of data. Got {!r}, expected {!r}".format(
+                    self.arraytype, time.shape, tuple(self._data.shape[0])
+                ))
+            self._time = time
+            self._timestep = np.diff(self._time[:2]) if np.all_close(np.diff(self._time),np.diff(self._time[:2])) else None
+        
         
     
     def apply_clip(self,data):
         """docstring for apply_clip"""
         if isinstance(self.clip,slice):
-            return data[self.clip,...]
+            return data[self.clip]
         else:
             return data
     
@@ -60,31 +77,13 @@ class WCAOTimeseries(WCAOEstimate):
     @property
     def time(self):
         """The time array"""
-        return np.arange(self.ntime) * self._timestep
+        return self.apply_clip(self._time)
         
-    def load(self,filename=None):
-        """Load the data from a numpy file."""
-        if filename is None:
-            filename = self.npyname
-        self._init_data(np.load(filename))
-        
-    def load_IDL(self,filename,method_index=0):
-        """Load the IDL format of this data."""
-        from astropy.io import fits
-        
-        with fits.open(filename) as HDUs:
-            data = HDUs[0].data.copy()[:,:2,method_index]
-            data = data[:,np.newaxis,:]
-        self._init_data(data)
-            
-            
-    def save(self,filename=None):
-        """Save the data to a numpy file."""
-        if filename is None:
-            filename = self.npyname
-        np.save(filename,self._data)
-            
-            
+    @property
+    def timestep(self):
+        """The step size of the time array, if it is well defined."""
+        return self._timestep
+    
     def smoothed(self,window,mode='flat'):
         """docstring for smoothed"""
         from aopy.util.math import smooth
@@ -94,98 +93,4 @@ class WCAOTimeseries(WCAOEstimate):
                 rv[:,layer,i] = self.apply_clip(smooth(self._data[:,layer,i],window,mode))
         return rv
     
-    def timeseries(self,ax,coord=0,smooth=dict(window=100,mode='flat'),rasterize=True,**kwargs):
-        """Plot a timeseries on the given axis."""
-        rv = []
-        if smooth:                
-            data = self.smoothed(**smooth)
-        else:
-            data = self.data
-        if coord < 2:
-            kwargs.setdefault('label',"xy"[coord])
-            for layer in range(self.nlayers):
-                rv += list(ax.plot(self.time,data[:,layer,coord],**kwargs))
-        else:
-            kwargs.setdefault('label',"magnitude")
-            mdata = np.sqrt(np.sum(data**2.0,axis=2))
-            for layer in range(self.nlayers):
-                rv += list(ax.plot(self.time,mdata[:,layer],**kwargs))
-        if rasterize and len(data) > 1e3:
-            [ patch.set_rasterized(True) for patch in rv ]
-        return rv
-        
-    def map(self,ax,smooth=None,**kwargs):
-        """Plot a histogram map"""
-        if smooth:
-            data = self.smoothed(**smooth)
-        else:
-            data = self.data
-        xlabel = kwargs.pop("xlabel",r"$v_x\; \mathrm{(m/s)}$")
-        ylabel = kwargs.pop("ylabel",r"$v_y\; \mathrm{(m/s)}$")
-        kwargs.setdefault("bins",51)
-        size = kwargs.pop("size",False)
-        if size:
-            kwargs["range"] = [[-size,size],[-size,size]]
-        title = kwargs.pop("label",r"{:s} \verb+{:s}+ {:s}".format(self.longname,self.case.casename,self.case.instrument.replace("_"," ")))
-        ax.set_title(title)
-        if xlabel:
-            ax.set_xlabel(xlabel)
-        if ylabel:
-            ax.set_ylabel(ylabel)
-        circles = kwargs.pop("circles",[10,20,30,40])
-        rv = []
-        counting = []
-        for layer in range(self.nlayers):
-            (counts, xedges, yedges, Image) = ax.hist2d(data[:,layer,0],data[:,layer,1],**kwargs)
-            rv.append(Image)
-        
-        if isinstance(circles,collections.Sequence):
-            rv += self._circles(ax,dist=circles)
-        return rv
-        
-    def threepanelts(self,fig,smooth=dict(window=100,mode='flat'),**kwargs):
-        """Do the basic threepanel plot"""
-        if len(fig.axes) != 3:
-            axes = [ fig.add_subplot(3,1,i+1) for i in range(3) ]
-            title = kwargs.pop("label",r"{:s} \verb+{:s}+ {:s}".format(self.longname,self.case.casename,self.case.instrument.replace("_"," ")))
-            fig.suptitle(title)
-        else:
-            axes = fig.axes
-        labels = ["$w_x$","$w_y$","$|w|$"]
-        for i in range(3):
-            label = labels[i]
-            self.timeseries(axes[i],coord=i,smooth=False,label="Wind {:s}".format(label),marker='.',alpha=0.1,ls='None',**kwargs)
-            self.timeseries(axes[i],coord=i,smooth=smooth,label="Wind {:s}".format(label),marker='None',ls='-',alpha=1.0,lw=2.0,**kwargs)
-            axes[i].set_title("Wind {:s}".format(label))
-            axes[i].set_ylabel("Speed (m/s)")
-            
-        lims = []
-        for i in range(2):
-            lims += list(axes[i].get_ylim())
-        
-        for i in range(3):
-            if i == 2:
-                ym,yp = axes[i].get_ylim()
-                axes[i].set_ylim(0.0,yp)
-                axes[i].set_xlabel("Time (s)")
-            else:
-                axes[i].set_ylim(min(lims),max(lims))
-        
-        return fig
-        
-    def _circles(self,ax,dist=10,origin=[0,0],color='w',crosshair=True):
-        """Show circles"""
-        from matplotlib.patches import Circle
-        from matplotlib.lines import Line2D
-        circles = [Circle(origin,R,fc='none',ec=color,ls='dashed',zorder=0.1) for R in dist]
-        if crosshair:
-            Rmax = max(dist)
-            major = [ -Rmax, Rmax ]
-            minor = [ 0 , 0 ]
-            coords = [ (major,minor), (minor,major)]
-            for xdata,ydata in coords:
-                circles.append(
-                    Line2D(xdata,ydata,ls='dashed',color=color,marker='None',zorder=0.1)
-                )
-        [ ax.add_artist(a) for a in circles ]
-        return circles
+
--- wcao/data/windmap.py
+++ wcao/data/windmap.py
@@ -18,57 +18,54 @@ from __future__ import (absolute_import, unicode_literals, division,
 import numpy as np
 
 import datetime
+import warnings
 import os.path
 
+from astropy.units import u
+
 from .estimator import WCAOEstimate
+
+from aopy.util.units import ensure_quantity
 
 
 class WCAOMap(WCAOEstimate):
     """A generic WCAO estimate as a map."""
     
-    def _init_data(self,data):
+    def init_data(self,data=None, vx=None, vy=None, layers=None):
         """Data initialization."""
         if isinstance(data, np.ndarray):
             if data.ndim == 2:
                 self.map = data
-                self.vx = np.arange(self.map.shape[0])
-                self.vy = np.arange(self.map.shape[1])
             else:
                 raise ValueError("{}: Expected map with ndim=2, got {:d}".format(
                     self, data.ndim
                 ))
-        elif isinstance(data, tuple):
-            if len(data) == 3:
-                self.map, self.vx, self.vy = data
-            elif len(data) == 4:
-                self.map, self.vx, self.vy, self.layers = data
-            else:
-                raise ValueError("{}: Data has the wrong number of elements: {:d}.".format(
-                    self, len(data)
-                ))
-        elif isinstance(data, dict):
-            self.map = data["map"]
-            self.vx = data.get("vx", np.arange(self.map.shape[0]))
-            self.vy = data.get("vy", np.arange(self.map.shape[1]))
-            self.layers = data.get("layers", [])
-    
-    def save(self):
-        """Save a file"""
-        from ..io.fitsmaps import MapIO
-        writer = MapIO(self.fitsname.rstrip(".fits"), self)
-        writer.write()
-    
-    def load(self):
-        """Load a file"""
-        from ..io.fitsmaps import MapIO
-        reader = MapIO(self.fitsname.rstrip(".fits"), self)
-        reader.read()
+        elif data is not None:
+            raise ValueError("{}: Expected map of type {}, got {}".format(
+                self, np.ndarray, type(data)
+            ))
+        else:
+            if not all(map(lambda i: i is None,[vx,vy,layers])):
+                warnings.warn("{}:Ignoring metadata because map==None".format(self))
+            return
         
+        if vx is None:
+            self.vx = np.arange(self.map.shape[0])
+        else:
+            self.vx = ensure_quantity(vx, u.m/u.s)
+        if vy is None:
+            self.vy = np.arange(self.map.shape[1])
+        else:
+            self.vy = ensure_quantity(vy, u.m/u.s)
+        self.layers = layers or []
     
     @property
     def extent(self):
         """The extent array for this map."""
         return [np.min(self.vx),np.max(self.vx),np.min(self.vy),np.max(self.vy)]
         
-    
+    @property
+    def data(self):
+        """Data accessor."""
+        return self.map
         
