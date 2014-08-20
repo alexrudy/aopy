f9f22a6128cb8abcee7e1cf9c2d881de6880a517
Alex Rudy <alex.rudy@gmail.com>
2014-08-20 13:14:12 -0700

    Minor feature improvements

---
+++ aopy/aperture/view.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+#  view.py
+#  aopy
+#
+#  Created by Alexander Rudy on 2014-07-16.
+#  Copyright 2014 Alexander Rudy. All rights reserved.
+#
+"""
+:mod:`aperture.view`
+====================
+
+
+
+"""
